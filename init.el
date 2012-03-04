7f6a6e6d4f77d2040f1ea55fd621ea816a7925ad
Seth Falcon <seth@userprimary.net>
2012-03-03 17:35:08 -0800

    Reformat and add custom.el

--- init.el
+++ init.el
@@ -1,13 +1,10 @@
 ;; Emacs 24 init.el
 ;; Based on http://github.com/eschulte/emacs24-starter-kit
 
-;; I'm experimenting with keeping my Emacs configuration in literate Org files.
-;; This bootstraps the whole thing.
-;;
 ;; NOTE: The name of the Org files is important!  When a file gets tangled,
 ;; it gets the same base name as the Org file.  Thus, tangling Emacs Lisp from
 ;; a file `init.org` would generate `init.el`, obliterating this file in the
-;; process!
+;; process. So your config org file should not be named "init.org".
 
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
