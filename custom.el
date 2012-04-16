9aae813c38554433dfd291f78c491aa75153f7a7
Seth Falcon <seth@opscode.com>
2012-04-16 09:42:51 -0700

    Default font and such in custom

--- custom.el
+++ custom.el
@@ -5,12 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(confluence-save-credentials t)
  '(confluence-save-page-minor-edits nil)
- '(flyspell-delay 6)
- '(flyspell-issue-message-flag nil)
- '(safe-local-variable-values (quote ((erlang-indent-level . 4)))))
+ '(safe-local-variable-values (quote ((erlang-indent-level . 4))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
