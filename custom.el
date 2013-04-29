6a021933b0cb219c2dbbd6276249b5b4259f6180
Seth Falcon <seth@opscode.com>
2013-04-29 11:38:23 -0700

    WIP

--- custom.el
+++ custom.el
@@ -3,13 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(confluence-save-credentials t)
- '(confluence-save-page-minor-edits nil)
- '(safe-local-variable-values (quote ((erlang-indent-level . 4))))
- )
+ '(projmake-project-descs (quote (("Rebar" "rebar.config" "nice -n5 rebar skip_deps=true compile") ("Make" "Makefile" "nice -n5 make compile")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
+ )
