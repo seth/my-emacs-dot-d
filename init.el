1450d0dc3760e96a2f63b13666ad404adfd354e3
Seth Falcon <seth@opscode.com>
2013-05-03 10:20:57 -0700

    Switch to built-in org-mode but use vendored to get contrib stuff

--- init.el
+++ init.el
@@ -7,6 +7,7 @@
 ;; process. So your config org file should not be named "init.org".
 
 (require 'ob-tangle)
+;; (setq debug-on-error t)
 (org-babel-load-file
  (expand-file-name "emacs-init.org"
                    user-emacs-directory))
