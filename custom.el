d81ebc814951a58dbd9d2341b871720d15eacdbc
Seth Falcon <seth@userprimary.net>
2014-06-06 23:49:17 -0700

    Major overhaul switching to use-package

--- custom.el
+++ custom.el
@@ -3,12 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(column-number-mode t)
- '(haskell-mode-hook (quote (turn-on-haskell-indentation)) t)
- '(show-paren-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default)))
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
