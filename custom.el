1f755e3e75c55089a99ef8569102243b6d353b20
Seth Falcon <seth@userprimary.net>
2014-06-06 21:42:02 -0700

    various

--- custom.el
+++ custom.el
@@ -3,13 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(confluence-save-credentials t)
- '(confluence-save-page-minor-edits nil)
- '(safe-local-variable-values (quote ((erlang-indent-level . 4))))
- )
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation)) t)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "black" :foreground "wheat" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco")))))
+ )
